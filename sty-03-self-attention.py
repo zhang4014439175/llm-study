4079359b4688f34164f5d9ffead3c2556dbde353
zhangzhiming <915219844@qq.com>
2024-12-20 17:39:35 +0800

    24-12-19: self-attention-class

--- sty-03-self-attention.py
+++ sty-03-self-attention.py
@@ -156,6 +156,8 @@ def trainable_weights():
     print(value_2)
 
     # 获取到输入字符的所有key值和value值
+    # keys 根据实时 W_key 来计算出当前字符的字符特征是什么
+    # values 根据实时 W_value 来计算出当前字符的内容是什么
     keys = inputs @ W_key
     values = inputs @ W_value
     print("keys.shape:", keys.shape)
@@ -166,18 +168,47 @@ def trainable_weights():
     attn_score_22 = query_2.dot(keys_2)
     print(attn_score_22)
 
-    # 所有的注意力分数
+    # 所有的注意力分数,点积计算注意力分数
     attn_scores_2 = query_2 @ keys.T
     print(attn_scores_2)
 
-    # 计算占比
+    # 归一化计算占比
     d_k = keys.shape[-1]
     attn_weights_2 = torch.softmax(attn_scores_2 / d_k ** 0.5, dim=-1)
     print(attn_weights_2)
+    print(values)
 
     # 得到 journey 的上下文向量
+    # values 提供了句子中每个单词的语义内容。
+    # attn_weights_2 是一种“放大镜”，告诉模型需要关注哪些内容。
+    # context_vec_2 是一个“总结”，它动态地结合了当前语境中最相关的信息。
     context_vec_2 = attn_weights_2 @ values
     print(context_vec_2)
+
+
+def use_class():
+    inputs = torch.tensor(
+        [[0.43, 0.15, 0.89],  # Your (x^1)
+         [0.55, 0.87, 0.66],  # journey (x^2)
+         [0.57, 0.85, 0.64],  # starts (x^3)
+         [0.22, 0.58, 0.33],  # with (x^4)
+         [0.77, 0.25, 0.10],  # one (x^5)
+         [0.05, 0.80, 0.55]]  # step (x^6)
+    )
+    torch.manual_seed(123)
+    d_in = inputs.shape[1]
+    d_out = 2
+
+    # SelfAttention_v1 and SelfAttention_v2 give different outputs because
+    # they use different initial weights for the weight matrices since nn.Linear uses a more
+    # sophisticated weight initialization scheme
+    from self_attention_v1 import SelfAttention_v1, SelfAttention_v2
+    sa_v1 = SelfAttention_v1(d_in, d_out)
+    print(sa_v1(inputs))
+
+    torch.manual_seed(789)
+    sa_v2 = SelfAttention_v2(d_in, d_out)
+    print(sa_v2(inputs))
 
 
 def softmax_naive(x):
@@ -190,4 +221,5 @@ def softmax_naive(x):
 
 
 if __name__ == '__main__':
-    trainable_weights()
+    # trainable_weights()
+    use_class()
