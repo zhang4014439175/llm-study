da1e71c69e4a5d44869bc920a0cb8f446089eff2
zhangzhiming <915219844@qq.com>
2025-01-06 17:42:27 +0800

    25-01-06: Pretraining on unlabeled data - Train

--- chapter05/pretraining.py
+++ chapter05/pretraining.py
@@ -36,7 +36,7 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(flat.tolist())
 
 
-def get_model():
+def get_model(seed=True):
     from chapter04.styc_04_dummy_gpt_model import GPTModel
     GPT_CONFIG_124M = {
         "vocab_size": 50257,
@@ -47,7 +47,8 @@ def get_model():
         "drop_rate": 0.1,
         "qkv_bias": False
     }
-    torch.manual_seed(123)
+    if seed:
+        torch.manual_seed(123)
     model = GPTModel(GPT_CONFIG_124M)
     model.eval()
     return model, GPT_CONFIG_124M
@@ -206,15 +207,16 @@ def pretrain():
         shuffle=False,
         num_workers=0
     )
-    print("Train loader:")
-    for x, y in train_loader:
-        print(x.shape, y.shape)
-    print("\nValidation loader:")
-    for x, y in val_loader:
-        print(x.shape, y.shape)
+    # print("Train loader:")
+    # for x, y in train_loader:
+    #     print(x.shape, y.shape)
+    # print("\nValidation loader:")
+    # for x, y in val_loader:
+    #     print(x.shape, y.shape)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # If you have a machine with a CUDA-supported GPU, the LLM will train on the GPU without making any changes to the code.
+    # If you have a machine with a CUDA-supported GPU, the LLM will train on the GPU without making any changes to
+    # the code.
     model.to(device)
     # Disables gradient tracking for efficiency because we are not training yet
     with torch.no_grad():
@@ -224,8 +226,130 @@ def pretrain():
     print("Training loss:", train_loss)
     print("Validation loss:", val_loss)
 
+    # train start
+    torch.manual_seed(123)
+    model.to(device)
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=0.0004, weight_decay=0.1
+    )
+    num_epochs = 10
+    train_losses, val_losses, tokens_seen = train_model_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+        start_context="Every effort moves you", tokenizer=tokenizer
+    )
+
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+    # transferring the model back from the GPU to the CPU
+    model.to("cpu")
+    model.eval()
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    token_ids = generate_text_simple(
+        model=model,
+        idx=text_to_token_ids("Every effort moves you", tokenizer),
+        max_new_tokens=25,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+def test():
+    # 暂时没用
+    model, GPT_CONFIG_124M = get_model()
+    model.to("cpu")
+    model.eval()
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    token_ids = generate_text_simple(
+        model=model,
+        idx=text_to_token_ids("Every effort moves you", tokenizer),
+        max_new_tokens=25,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import MaxNLocator
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(
+        epochs_seen, val_losses, linestyle="-.", label="Validation loss"
+    )
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
+
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    # It prints the training and validation set losses after each model update so we can evaluate whether the
+    # training improves the model. More specifically, the evaluate_model function calculates the loss over the
+    # training and validation set while ensuring the model is in evaluation mode with gradient tracking and dropout
+    # disabled when calculating the loss over the training and validation sets:
+
+    # Dropout is disabled during evaluation for stable,reproducible results.
+    model.eval()
+    # Disables gradient tracking, which is not required during evaluation, to reduce the computational overhead
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    # we use to track whether the model improves during the training. In particular, the generate_and_print_sample
+    # function takes a text snippet (start_context) as input, converts it into token IDs, and feeds it to the LLM to
+    # generate a text sample using the generate_text_simple function we used earlier:
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(model=model, idx=encoded, max_new_tokens=50, context_size=context_size)
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter,
+                       start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch + 1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}"
+                      )
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
 
 if __name__ == '__main__':
     # init_gpt()
     # no01_calculating_the_text_generation_loss()
     pretrain()
+    # test()
